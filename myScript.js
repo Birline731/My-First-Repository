fedffd8c2686d9f2c91165440fd5ce81a5a2f34b
Birline731 <beunca37@gmail.com>
2024-06-21 17:05:09 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -8,3 +8,10 @@ function hideImage() {
   // Set the display style property to 'none' to hide the image
   image.style.display = 'none';
 }
+
+// Function to show the image
+function showImage() {
+  var image = document.getElementById('myImage');
+  // Set the display style property to 'block' to make the image appear
+  image.style.display = 'block';
+}
