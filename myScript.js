7b60c335ea5edcaa34fc3238d26877c59f0c6e6b
Birline731 <beunca37@gmail.com>
2024-06-21 17:16:49 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -9,9 +9,9 @@ function hideImage() {
   image.style.display = 'none';
 }
 
-// Function to show the image
-function showImage() {
+// Function to change the image
+function changeImage() {
   var image = document.getElementById('myImage');
-  // Set the display style property to 'block' to make the image appear
-  image.style.display = 'block';
+    image.src = 'https://i.etsystatic.com/33206174/r/il/2f797c/3581278854/il_fullxfull.3581278854_nv7b.jpg';  // Replace with your second image path
+
 }
