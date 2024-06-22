408d564776b6e9719513680be2615fc88083e911
Birline731 <beunca37@gmail.com>
2024-06-22 13:36:40 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -7,7 +7,7 @@ function myFunction() {
 
 function changeImage() {
     var image = document.getElementById('myImage');
-    if (image.src.match('mykingcurry.jpg')) {
+    if (image.src.match('original-image.jpg')) {
         image.src = 'https://m.media-amazon.com/images/I/51d23FXg2PL._AC_UF894,1000_QL80_.jpg';  // Replace with the path to your new image
     } else {
         image.src = 'mykingcurry.jpg';  // Restore original image
