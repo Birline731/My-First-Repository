ebdc2cf12217cb76468db42c8c3ed95974656eaa
Birline731 <beunca37@gmail.com>
2024-06-22 13:00:32 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,12 +2,6 @@ function myFunction() {
     document.getElementById("demo").innerHTML="kidus sucks at basketball.";
 }
 
-// Function to hide the image
-function hideImage() {
-  var image = document.getElementById('myImage');
-  // Set the display style property to 'none' to hide the image
-  image.style.display = 'none';
-}
 
 // Function to change the image
 function changeImage() {
